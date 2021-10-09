12f8b6176d9f84217a978f198792f558b37785c9
Kylian <hsjgc1234@gmail.com>
2021-10-09 14:14:06 +0900

    signup 헤더 추가

--- SNScommunity/SignUp.h
+++ SNScommunity/SignUp.h
@@ -26,3 +26,32 @@ public:
         this->pw = pw;
     }
 };
+
+void signUpUser(User user){     //  회원가입에 쓰이는 함수, User 객체를 받아와서 파일에 맨 뒤에다가 입력
+    string id = user.getId();
+    string pw = user.getPw();
+    fstream member_list;
+    member_list.open("member_information.txt", ios::app);
+    member_list << id;
+    member_list.put('/');
+    member_list << pw;
+    member_list.put('\n');
+}
+
+bool isExistUser(User user){        //  회원가입 전에 이미 등록된 아이디가 있는지 확인하는 함수
+    string id = user.getId();
+    // string pw = user.getPw();
+    fstream member_list;
+    member_list.open("member_infromation.txt", ios::in);
+    string member_info;     //  member_info -> buffer
+    bool flag = false;
+    while(getline(member_list, member_info)){
+        int id_idx = member_info.find_first_of('/');
+        string compare_id = member_info.substr(0, id_idx);
+        if(id == compare_id){
+            flag = true;
+            break;
+        }
+    }
+    return flag;
+}
