dae481b6e2ae4f31d7966197cc23a7c2dadac83c
unknown <sikk806@gmail.com>
2021-10-23 18:26:46 +0900

    mainPost

--- SNScommunity/board.h
+++ SNScommunity/board.h
@@ -9,18 +9,36 @@
 #include <sstream>
 #include <stdlib.h>
 
+enum {
+    BLACK,
+    DARK_BLUE,
+    DARK_GREEN,
+    DARK_SKYBLUE,
+    DARK_RED,
+    DARK_VOILET,
+    DAKR_YELLOW,
+    GRAY,
+    DARK_GRAY,
+    BLUE,
+    GREEN,
+    SKYBLUE,
+    RED,
+    VIOLET,
+    YELLOW,
+    WHITE,
+};
+
 using namespace std;
 
 class Board {
 private:
 
-
 public:
 	Board();
 	~Board();
 	void selectCategory(string user_id); // 카테고리 선택
-	void mainPost(string _userid, string _category); // 글 목록
-	void createPost(string _userid, string _category); // 글 작성
+	void mainPost(string _userid, string _category, int _getFile); // 글 목록
+    void setColor(unsigned short text); // windows.h를 사용하는 함수 Mac에서 불가능할 경우 삭제.
 
 	int numFile(string _category); // 폴더내 파일 갯수
 };
